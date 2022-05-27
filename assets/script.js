e0067e5efb5e39016d0df04404adee31df6ab5fc
diegorodd <dieegoa@icloud.com>
2022-05-26 20:07:33 -0400

    input

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,11 @@
-console.log(window.document);
-
-window.document.querySelector("button");
-
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-
-document.querySelector("button").textContent;
-
-buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
-
-  document.createElement("li");
-
-  var taskItemEl = document.createElement("li");
-
-  var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() { 
-    var listItemEl = document.createElement("li"); 
-    listItemEl.className = "task-item"; 
-    listItemEl.textContent = "This is a new task."; 
-    tasksToDoEl.appendChild(listItemEl); 
-    }; 
-  
-  buttonEl.addEventListener("click", createTaskHandler);
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
+};
 
-// buttonEl.addEventListener("click", function() {
-//   var listItemEl = document.createElement("li");
-//   listItemEl.className = "task-item";
-//   listItemEl.textContent = "This is a new task.";
-//   tasksToDoEl.appendChild(listItemEl);
-// });
-
-// var createTaskHandler = function() {
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "task-item";
-//     listItemEl.textContent = "This is a new task.";
-//     tasksToDoEl.appendChild(listItemEl);
-//   }
+buttonEl.addEventListener("click", createTaskHandler);
