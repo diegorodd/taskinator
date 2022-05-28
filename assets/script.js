4317024e7654b181502ffe01449c36eaa62ae8fd
diegorodd <dieegoa@icloud.com>
2022-05-27 22:14:33 -0400

    fixed #

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,7 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
-var pageContentEl = document.querySelector("#page-content");
+var pageContentEl = document.querySelector(".page-content");
 
 var taskFormHandler = function(event) {
   event.preventDefault();
